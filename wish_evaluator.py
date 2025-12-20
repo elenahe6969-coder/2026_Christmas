4e7f8220dee07742c06e21a939a78de3a54284c3
Coderelena <elenahe6969@gmail.com>
2025-12-20 17:53:07 +0800

    Update wish_evaluator.py

--- wish_evaluator.py
+++ wish_evaluator.py
@@ -527,7 +527,7 @@ if shared_wish_id:
 
     # Make your own wish section with compact spacing
     st.markdown("---")
-    st.markdown("### 🎄 Make Your Own Wish?")
+    st.markdown("###Make Your Own Wish?")
     st.markdown('<div class="center-content">', unsafe_allow_html=True)
     st.markdown("https://2026christmas-yourwish-mywish-elena.streamlit.app/")
     st.markdown('</div>', unsafe_allow_html=True)
